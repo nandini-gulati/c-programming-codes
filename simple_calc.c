6cb55c5d482ff2260fb6aade5f1120e3c572a665
nandini-gulati <146436819+nandini-gulati@users.noreply.github.com>
2023-10-05 11:07:34 +0530

    code to make a simple caclulator using switch case

---
+++ simple_calc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+char op;
+double n1, n2;
+printf("Enter an operator (+, -, *, /):");
+scanf("%c", &op);
+printf("Enter two operands: ");
+scanf("%lf %lf", &n1, &n2);
+switch (op) 
+{
+    case '+':
+        printf("%.lf + %.lf = = %.lf", n1,n2,n1+n2); 
+        break;
+
+    case '-':
+        printf("%.lf - %.lf %.lf", n1,n2,n1-n2);
+        break;
+
+    case '*':
+        printf("%.lf*%.lf %.lf", n1,n2,n1*n2);
+        break;
+
+    case '/':
+        printf("%.lf / %.lf = %.lf", n1,n2,n1/n2);
+        break;
+
+    default:
+        printf("Error! operator is not correct");
+}
+
+return 0;
+}
